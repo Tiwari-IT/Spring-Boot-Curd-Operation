b8b1bbd7921fed398210b4a59bcfbfe4592538e4
Tiwari-IT <ehtiwarig@gmail.com>
2023-08-26 04:38:03 +0530

    first commit

---
+++ src/main/java/com/vip/repo/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.vip.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vip.entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into employees values(10001, 'Vipul', 'Tiwari', 'ehvipul@gmail.com');
+insert into employees values(10002, 'Rock', 'Singh', 'ehvipul@gmail.com');
+insert into employees values(10003, 'Vimal', 'Tiwari', 'ehvipul@gmail.com');
+insert into employees values(10004, 'prakash', 'Tiwari', 'ehvipul@gmail.com');
+insert into employees values(10005, 'Vipeen', 'Tiwari', 'ehvipul@gmail.com');
+insert into employees values(10006, 'Vinay', 'Tiwari', 'ehvipul@gmail.com');
+insert into employees values(10007, 'Vishal', 'Tiwari', 'ehvipul@gmail.com');
+insert into employees values(10008, 'Vivek', 'Tiwari', 'ehvipul@gmail.com');
